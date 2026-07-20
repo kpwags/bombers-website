9c16c6cc9710edd5a9a1772463271036290ab64a
Keith Wagner <github@kpwags.com>
2026-07-20 09:04:54 -0400

    Added All Seasons and Games to Current

---
+++ eleventy.config.images.js
@@ -0,0 +1,31 @@
+import path from 'path';
+import eleventyImage from '@11ty/eleventy-img';
+
+export default function (eleventyConfig) {
+    function relativeToInputPath(inputPath, relativeFilePath) {
+        let split = inputPath.split('/');
+        split.pop();
+
+        return path.resolve(split.join(path.sep), relativeFilePath);
+    }
+
+    eleventyConfig.addAsyncShortcode('image', async function imageShortcode(src, alt, widths, sizes) {
+        let formats = ['webp', 'gif', 'auto'];
+        let file = relativeToInputPath(this.page.inputPath, src);
+        let metadata = await eleventyImage(file, {
+            widths: widths || ['auto'],
+            formats,
+            sharpOptions: { animated: true },
+            outputDir: path.join(eleventyConfig.dir.output, 'img'),
+        });
+
+        // TODO loading=eager and fetchpriority=high
+        let imageAttributes = {
+            alt,
+            sizes,
+            loading: 'lazy',
+            decoding: 'async',
+        };
+        return eleventyImage.generateHTML(metadata, imageAttributes);
+    });
+}
